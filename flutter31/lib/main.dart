d8fa2a2b866a60eafc44d896a81192f9dab8d5b8
Dini Damayanti <dini.gamelab@gmail.com>
2023-11-09 01:35:11 +0700

    flutter31
    cupertino 33

--- flutter31/lib/main.dart
+++ flutter31/lib/main.dart
@@ -1,88 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:core';
+
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  List<Tab> myTab = [
-    Tab(
-      text: "Tab 1",
-      icon: Icon(Icons.add_a_photo),
-    ),
-    Tab(
-      text: "Tab 2",
-      icon: Icon(Icons.ac_unit_outlined),
-    ),
-    Tab(
-      text: "Tab 3",
-    ),
-    /*Tab(
-                  child: Text("data"),
-                ), */
-  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        initialIndex: 2,
-        length: myTab.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("My Apps"),
-            bottom: TabBar(
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.white,
-              labelStyle: TextStyle(fontWeight: FontWeight.bold),
-              unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
-              //ini hanya dibawahnya
-              /* indicatorColor: Colors.black,
-              indicatorWeight: 5,
-              indicatorPadding: EdgeInsets.all(10 ), */
-              indicator: BoxDecoration(
-                color: Colors.pink,
-                // borderRadius: BorderRadius.circular(50), //customer castum
-                border: Border(
-                  bottom: BorderSide(
-                    //bisa pakai top
-                    color: Colors.black,
-                    width: 5,
-                  ),
-                ),
-              ),
-              tabs: myTab,
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Center(
-                child: Text(
-                  "Tab 1",
-                  style: TextStyle(
-                    fontSize: 40,
-                  ),
-                ),
-              ),
-              Center(
-                child: Text(
-                  "Tab 2",
-                  style: TextStyle(
-                    fontSize: 40,
-                  ),
-                ),
-              ),
-              Center(
-                child: Text(
-                  "Tab 3",
-                  style: TextStyle(
-                    fontSize: 40,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      title: 'Design Comparison',
+      home: MyHomePage(),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime selectedDate = DateTime.now;
+
+  Future<void> _showMaterialDatePicker() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+  _showCupertinoDatePicker() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext builder) {
+        return Container(
+          height: 200,
+          child: CupertinoDatePicker(
+            initialDateTime: selectedDate,
+            onDateTimeChanged: (DateTime newDateTime) {
+              setState(() {
+                selectedDate = newDateTime;
+              });
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  _showMaterialAlertDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Material Alert'),
+          content: Text('This is a Material Design Alert Dialog.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _showCupertinoAlertDialog() {
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: Text('Cupertino Alert'),
+          content: Text('This is a Cupertino Design Alert Dialog.'),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Design Comparison'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Selected Date: ${selectedDate.toLocal()}'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _showMaterialDatePicker,
+              child: Text('Material Date Picker'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _showCupertinoDatePicker,
+              child: Text('Cupertino Date Picker'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _showMaterialAlertDialog,
+              child: Text('Material Alert Dialog'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _showCupertinoAlertDialog,
+              child: Text('Cupertino Alert Dialog'),
+            ),
+          ],
+        ),
+     ),
+);
+}
 }
